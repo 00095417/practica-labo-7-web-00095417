462f33d97ea676be71064bd8ad2d00ee35add087
00095417 <00095417@uca.edu.sv>
2018-10-12 08:27:03 -0600

    uso de js en el html

--- Practica/asset/js/script.js
+++ Practica/asset/js/script.js
@@ -10,3 +10,8 @@ container.style.height = "200px";
 container.innerHTML="<input type='text' placehorder='escriba su texto'>";
 
 var containers = document.getElementsByClassName("c2");
+
+containers = document.querySelectorAll(".c2");
+containers.forEach(element =>{
+    element.style.backgroundColor = "#009999";
+});
