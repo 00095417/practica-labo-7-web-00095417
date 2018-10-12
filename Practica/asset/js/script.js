29b50ed7f1f1c7d19a37cefdeb0ccf0a1cc244fc
00095417 <00095417@uca.edu.sv>
2018-10-12 08:36:47 -0600

    uso de js en el html

--- Practica/asset/js/script.js
+++ Practica/asset/js/script.js
@@ -14,4 +14,18 @@ var containers = document.getElementsByClassName("c2");
 containers = document.querySelectorAll(".c2");
 containers.forEach(element =>{
     element.style.backgroundColor = "#009999";
+    element.innerHTML="<input type='text' placehorder='escriba su texto'>";
 });
+
+var boton = document.getElementById("btn-click");
+
+boton.onclick = function(event){
+    alert("Hola mundo");
+}
+
+var btnCopy = document.getElementById("btnCopy");
+btnCopy.onclick = function(event){
+    let msj = document.getElementById('textMsj').value;
+    document.getElementById('showMsj').innerText = msj;
+}
+
